1c534a15a7b1a925f3dc1c84bc854a3a0681792b
RAGHAVENDRA-VAM <raghavendra.lakkamaraju@valuemomentum.com>
2026-05-06 12:43:16 +0000

    Add Terraform modules for webapp on azure

--- Shashank-Repo/webapp/variables.tf
+++ Shashank-Repo/webapp/variables.tf
@@ -15,27 +15,3 @@ variable "resource_group_name" {
   type        = string
   default     = "hari_check1"
 }
-
-variable "sku_name" {
-  description = "App Service Plan SKU (e.g., B1, B2, S1, P1v3, P2v3)"
-  type        = string
-  default     = "B1"
-}
-
-variable "os_type" {
-  description = "OS type: Linux or Windows"
-  type        = string
-  default     = "Linux"
-}
-
-variable "worker_count" {
-  description = "Number of workers in the App Service Plan"
-  type        = number
-  default     = 1
-}
-
-variable "zone_balancing_enabled" {
-  description = "Enable zone balancing (requires Premium SKU)"
-  type        = bool
-  default     = false
-}
