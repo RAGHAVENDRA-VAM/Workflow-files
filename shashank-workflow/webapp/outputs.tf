c9f1dae4c2b174c7aa883482ceef0c0279dca77d
RAGHAVENDRA-VAM <raghavendra.lakkamaraju@valuemomentum.com>
2026-05-14 21:50:16 +0530

    Refactor outputs.tf for cleaner formatting and add web_app_id

--- shashank-workflow/webapp/outputs.tf
+++ shashank-workflow/webapp/outputs.tf
@@ -1,13 +1,15 @@
-# outputs.tf
-
 output "web_app_name" {
-  value = var.os_type == "Linux" ?
-    azurerm_linux_web_app.this[0].name :
-    azurerm_windows_web_app.this[0].name
+  value = var.os_type == "Linux" ? azurerm_linux_web_app.this[0].name : azurerm_windows_web_app.this[0].name
+}
+
+output "web_app_id" {
+  value = var.os_type == "Linux" ? azurerm_linux_web_app.this[0].id : azurerm_windows_web_app.this[0].id
 }
 
 output "web_app_url" {
-  value = var.os_type == "Linux" ?
-    azurerm_linux_web_app.this[0].default_hostname :
-    azurerm_windows_web_app.this[0].default_hostname
+  value = var.os_type == "Linux" ? azurerm_linux_web_app.this[0].default_hostname : azurerm_windows_web_app.this[0].default_hostname
+}
+
+output "identity_principal_id" {
+  value = var.os_type == "Linux" ? azurerm_linux_web_app.this[0].identity[0].principal_id : azurerm_windows_web_app.this[0].identity[0].principal_id
 }
